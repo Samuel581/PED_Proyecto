69e8301aa8a73dfb8ec18a37fd4d2d99d8506d01
Samuel581 <62672937+Samuel581@users.noreply.github.com>
2020-11-11 19:20:14 -0600

    Modificacion grafica del menu

--- proyecto.cpp
+++ proyecto.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Pelicula{
     string nombrePelicula;
     float precio = 3.50;
-    int cantidad=0;
+    int cantidad = 0;
 };
 
 typedef struct Pelicula pelicula;
@@ -34,26 +34,50 @@ int main(){
     cout << "| #       #    # #    # # #        #         #       # #      #    # #    #  |" << endl;
     cout << "| #       #    # #    # # ######   #         #       # ###### #    #  ####   |" << endl;
     cout << " ----------------------------------------------------------------------------" << endl << endl;
-    int opcion = 0;//Incializando la variable opcion
-    bool estado = true;//Inizializando un bool para usarlo como contunuacion o stop del menu
-    while(estado){
+    int opcionMenuP{0}, opcionTran{0};//Incializando la variable opcion
+    bool estadoMenuP{true}, estadoTran{true};//Inizializando un bool para usarlo como contunuacion o stop del menu
+    while(estadoMenuP){
+        cout << "   - | Menu principal | -\n" << endl;
         cout <<"1. Hacer una transaccion" << endl;
         cout <<"2. Ver las categorias mas populares" << endl;
         cout <<"3. Calcular ganancias totales" << endl;
         cout <<"4. Salir" << endl;
-        cout << ">>>> ";
-        cin >> opcion;//Obtener accion a realizar
+        cout << "Seleccione accion a realizar: ";
+        cin >> opcionMenuP;//Obtener accion a realizar
         //Menu
-        switch(opcion){
+        switch(opcionMenuP){
             case 1:
+            while(estadoTran){
+                cout << "\n     | Transaccion |\n" << endl;
+                cout << "1. Pelicula de Accion\n2.Pelicula de Drama\n3.Pelicula de Comedia\n4.Terminar transaccion" << endl;
+                cout << "Seleccione accion a realizar: ";
+                cin >> opcionTran;
+                switch(opcionTran){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                    estadoTran = false;
+                        break;
+                    default:
+                    cout << "Seleccione una opcion valida " << endl;
+                        break;
+                }
+                cout << endl;
+            }
                 break;
             case 2:
+            cout << "     | Categorias mas populares | " << endl;
                 break;
             case 3:
+            cout << "     | Ganancias totales |" << endl;
                 break;
             case 4:
-            cout << "Gracias por usar nuestro software!" << endl;
-            estado = false;
+            cout << "\nGracias por usar nuestro software!" << endl;
+            estadoMenuP = false;
                 break;
             default:
             cout << "\nSeleccione una opcion valida!\n" << endl;
@@ -82,6 +106,7 @@ int cant(){
             break;
         }else{
             cout<<"Ingrese una cantidad de tickets valida"<<endl;
+        }
     }
     return x;
 }
