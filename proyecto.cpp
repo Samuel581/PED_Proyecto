1302232638c7382a3caf5b062a4b43f984ccc6d8
Samuel581 <62672937+Samuel581@users.noreply.github.com>
2020-11-18 22:32:02 -0600

    Implementacion de nuevo logo y acentuacion

--- proyecto.cpp
+++ proyecto.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-//Estructura utilizada para realizar el ranking de generos mas vistos
+//Estructura utilizada para realizar el ranking de géneros más vistos
 struct sortGen{
     int cant_tick = 0;
     string id;
@@ -29,7 +29,7 @@ struct transaccion{
 
 typedef struct transaccion nodo; // Creamos un tipo definido para struct transaccion
 
-list<nodo> accion; //Inicializamos las listas donde se guardaran los ticket vendidos
+list<nodo> accion; //Inicializamos las listas donde se guardaran los tickets vendidos
 list<nodo> drama;
 list<nodo> comedia;
 void push_backL(string nombre, string nombreP, int cantidad, list<nodo> *s);
@@ -43,26 +43,27 @@ void ganancia();
 float gananciastotales=0;
 
 int main(){
-    setlocale(LC_ALL, "");  //Funcion utilizada para agregar caracteres especiales: palabras tildadas
-    cout << " ----------------------------------------------------------------------------" << endl;
-    cout << "| #######                                    #######                         |" << endl;
-    cout << "| #         ##   #    # # #     #     #      #       # #      #    #  ####   |" << endl;
-    cout << "| #        #  #  ##  ## # #      #   #       #       # #      ##  ## #       |" << endl;
-    cout << "| #####   #    # # ## # # #       # #        #####   # #      # ## #  ####   |" << endl;
-    cout << "| #       ###### #    # # #        #         #       # #      #    #      #  |" << endl;
-    cout << "| #       #    # #    # # #        #         #       # #      #    # #    #  |" << endl;
-    cout << "| #       #    # #    # # ######   #         #       # ###### #    #  ####   |" << endl;
-    cout << " ----------------------------------------------------------------------------" << endl << endl;
-    int opcionMenuP{0}, opcionTran{0}; string client_name;//Incializando la variable opcion
-    bool estadoMenuP{true}, estadoTran;//Inizializando un bool para usarlo como continuacion o stop del menu
+    setlocale(LC_ALL, "");  //Función utilizada para agregar caracteres especiales: palabras tildadas
+    cout << R"(
+    ---------------------------------------------------------------
+        ______                _ __         _______ __              
+       / ____/___ _____ ___  (_) /_  __   / ____(_) /___ ___  _____
+      / /_  / __ `/ __ `__ \/ / / / / /  / /_  / / / __ `__ \/ ___/
+     / __/ / /_/ / / / / / / / / /_/ /  / __/ / / / / / / / (__  ) 
+    /_/    \__,_/_/ /_/ /_/_/_/\__, /  /_/   /_/_/_/ /_/ /_/____/  
+                              /____/                               
+    ---------------------------------------------------------------
+    )" << endl;
+    int opcionMenuP{0}, opcionTran{0}; string client_name;//Inicializando la variable opcion
+    bool estadoMenuP{true}, estadoTran;//Inicializando un bool para usarlo como continuación o stop del menú
     while(estadoMenuP){
         estadoTran=true;
-        cout << "   - | Menu principal | -\n" << endl;
+        cout << "   - | Menú principal | -\n" << endl;
         cout <<"1. Hacer una transacción" << endl;
-        cout <<"2. Ver las categorias mas populares" << endl;
+        cout <<"2. Ver las categorías más populares" << endl;
         cout <<"3. Calcular ganancias totales" << endl;
         cout <<"4. Salir" << endl;
-        cout << "Seleccione accion a realizar: ";
+        cout << "Opción a escoger: ";
         cin >> opcionMenuP;//Obtener accion a realizar
         //Menu
         switch(opcionMenuP){
@@ -72,8 +73,8 @@ int main(){
             getline(cin, client_name);
             while(estadoTran){
                 cout << "\n     | Transaccion |\n" << endl;
-                cout << "1. Pelicula de Accion\n2. Pelicula de Drama\n3. Pelicula de Comedia\n" << endl;
-                cout << "Seleccione accion a realizar: ";
+                cout << "1. Pelicula de Acción\n2. Pelicula de Drama\n3. Pelicula de Comedia\n" << endl;
+                cout << "Opción a escoger: ";
                 cin >> opcionTran;
                 switch(opcionTran){
                     case 1:
@@ -89,7 +90,7 @@ int main(){
                     estadoTran=confirmar();
                         break;
                     default:
-                    cout << "Seleccione una opcion valida " << endl;
+                    cout << "Seleccione una opción válida " << endl;
                         break;
                 }
                 cout << endl;
@@ -97,7 +98,7 @@ int main(){
                 break;
             case 2:
             cout<<endl;
-            cout << "     | Categorias mas populares | " << endl;
+            cout << "     | Categorías más populares | " << endl;
             cout<<endl;
             TicketsPorCategoria();
             cout<<endl;
@@ -110,11 +111,11 @@ int main(){
             cout << endl;
                 break;
             case 4:
-            cout << "\nGracias por usar nuestro software!" << endl;
+            cout << "\n¡Gracias por usar nuestro software!" << endl;
             estadoMenuP = false;
                 break;
             default:
-            cout << "\nSeleccione una opcion valida!\n" << endl;
+            cout << "\nSeleccione una opción válida\n" << endl;
                 break;
         }
     }
@@ -124,10 +125,10 @@ void accionMenu(string nombreCliente){ //Menu de peliculas de accion
     int opc, canTicket;
     bool estado = true;
     while(estado){
-    cout << "\n     | Peliculas de accion |\n" << endl;
-                cout << "El precio para todas las peliculas es de: $3.50\n\n";
+    cout << "\n     | Películas de acción |\n" << endl;
+                cout << "El precio para todas las películas es de: $3.50\n\n";
                 cout << "1. The Karate Kid\n2. Avengers: Endgame\n3. Spiderman: Un nuevo universo\n4. Dragon Ball Super: Broly" << endl;
-                cout << "Seleccione accion a realizar: ";
+                cout << "Opción a escoger: ";
                 cin >> opc;
                 switch(opc){
                     case 1:
@@ -166,10 +167,10 @@ void dramaMenu(string nombreCliente){ //Menu de peliculas de drama
     int opc, canTicket;
     bool estado = true;
     while(estado){
-    cout << "\n     | Peliculas de drama |\n" << endl;
-                cout << "El precio para todas las peliculas es de: $3.50\n\n";
+    cout << "\n     | Películas de drama |\n" << endl;
+                cout << "El precio para todas las películas es de: $3.50\n\n";
                 cout << "1. Bajo la misma estrella\n2. Extraordinario\n3. Una voz silenciosa\n4. Your name" << endl;
-                cout << "Seleccione accion a realizar: ";
+                cout << "Opción a escoger: ";
                 cin >> opc;
                 switch(opc){
                     case 1:
